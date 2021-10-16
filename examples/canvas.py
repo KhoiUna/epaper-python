edff2532056616ad9bcb14e7c97c22c64b073fd6
KhoiNguyen <thorwaitson@gmail.com>
2021-10-15 20:45:30 -0500

    update

--- examples/canvas.py
+++ examples/canvas.py
@@ -8,6 +8,7 @@ def get_assignments():
 
     course_id = '64066'  #course id
 
+    # Assignments API: List assignments
     webpage = requests.get('https://una.instructure.com/api/v1/courses/' +
                            course_id + '/assignments', headers={"Authorization": "Bearer " + token})
     soup = BeautifulSoup(webpage.content, features="html.parser")
@@ -32,4 +33,35 @@ def get_assignments():
     return output_string
 
 
+def get_course_info():
+    file = open('canvas_token.env.txt', 'r')
+    token = file.read()
+    file.close()
+
+    course_id = '64066'  # course id
+
+    # Courses API: Get a single course
+    # /api/v1/accounts/:account_id/courses/:id
+    webpage = requests.get('https://una.instructure.com/api/v1/courses/'+ 
+                            course_id, headers={"Authorization": "Bearer " + token})
+    soup = BeautifulSoup(webpage.content, features="html.parser")
+
+    js = {}
+    for i in soup:
+        js = json.loads(i)
+
+    return {"course_code": js["course_code"], "start_at": js["start_at"][:10], "end_at": js["end_at"][:10]}
+
+def get_professor_info():
+    webpage = requests.get("https://cooking-reservation.herokuapp.com/api/rasp")
+    soup = BeautifulSoup(webpage.content, features="html.parser")
+
+    js = {}
+    for i in soup:
+        js = json.loads(i)
+
+    return js
+
 # print(get_assignments())
+# print(get_course_info())
+# print(get_professor_info())
