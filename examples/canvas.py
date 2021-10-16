693a4e8df4098a166ada733b0f40946686564255
KhoiNguyen <thorwaitson@gmail.com>
2021-10-15 20:00:17 -0500

    update

--- examples/canvas.py
+++ examples/canvas.py
@@ -12,20 +12,24 @@ def get_assignments():
                            course_id + '/assignments', headers={"Authorization": "Bearer " + token})
     soup = BeautifulSoup(webpage.content, features="html.parser")
 
-    output_string = ''
-    count = 1
+    assignment_list= []
     for i in soup:
         js = json.loads(i)
-        for k in js:
-            # print(k['name'])
-            # print(k['due_at'])
-            output_string += '- Title: ' + \
-                k['name'] + '\n  Due date: ' + k['due_at'][:10] + '\n\n'
-            count += 1
-            if count > 3:
-                break
+        assignment_list.append(js)
+
+    output_string = ''
+    count = 1
+    for k in assignment_list[0][::-1]:
+        # print(k['name'])
+        # print(k['due_at'])
+        output_string += '- Title: ' + \
+            k['name'] + '\n  Due date: ' + k['due_at'][:10] + '\n\n'
+        count += 1
+        if count > 3:
+            break
+     
 
     return output_string
 
 
-print(get_assignments())
+# print(get_assignments())
